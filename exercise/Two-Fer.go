544765d1143a0fde6481b069e1795bef6c7a5dbf
mohammadreza yousefi <94249513+mrezayousefi@users.noreply.github.com>
2025-08-11 20:42:18 +0000

    change exercise

---
+++ exercise/Two-Fer.go
@@ -0,0 +1,24 @@
+package exercise
+
+import "fmt"
+
+func twofer(name string) string {
+	if name == "" {
+		return "One for you, one for me."
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
+}
+
+// The main function is not part of the twofer package, but is included here for testing purposes.
+// It can be removed when using the package in a larger application.
+
+// main function to demonstrate the twofer functionality
+
+func main() {
+	fmt.Println("Buy one bread, get two breads")
+	var count string
+	func(persion) {
+
+	}
+
+}
